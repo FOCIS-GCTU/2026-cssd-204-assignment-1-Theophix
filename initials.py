3ecc25adf974722457d31296a5f21d1b88ac2ee4
Theophix <108023127+Theophix@users.noreply.github.com>
2026-05-30 07:12:39 +0000

    Assignment

--- initials.py
+++ initials.py
@@ -1,7 +1,27 @@
+#File: initials.py
+#Description:   Print out my initials in block letters
+#Assignment Number: 1
+#
+#Name: Theophilus Mawuko Sewornu
+#STUDENT ID: 2425400343
+#Email: 2425400343@live.gctu.edu.gh
+#Grader: Augustus
+#
+#On my honor, Theophilus Mawuko Sewornu, this programming assignment is my own work
+#and i have not provided this code to any other student.
 
 def main():
-  pass #code goes here
-
-
-
+    # This main function prints the initials of my name in block letters
+    print('...TMS')
+    print('')
+    print('...TTTTTTTTTTTT........MMMM....MMMM..........SSSSSSSSSS.....')
+    print('...TTTTTTTTTTTT........MMMM....MMMM.........SSSSSSSSSSS.....')
+    print('.......TTTT............MM.MM..MM.MM........SSSS.............')
+    print('.......TTTT............MM.MM..MM.MM........SSSS.............')
+    print('.......TTTT............MM.MM..MM.MM..........SSSSSS.........')
+    print('.......TTTT............MM..MMMM..MM.............SSSSSS......')
+    print('.......TTTT............MM..MMMM..MM................SSSS.....')
+    print('.......TTTT............MM...MM...MM................SSSS.....')
+    print('.......TTTT.......**...MM...MM...MM...**....SSSSSSSSSSS.....')
+    print('.......TTTT.......**...MM...MM...MM...**....SSSSSSSSSS......')   
 main()
